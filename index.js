96a846a775add766427c5887b3be025d01ce45e8
Aakash Phogat <aakashphogat16@gmail.com>
2024-01-25 15:13:31 +0530

    over the air

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import CustomComponent from './src/screens/CustomNotifee/CustomComponent';
-import codePush from 'react-native-code-push';
 
-AppRegistry.registerComponent(appName, () => codePush(App));
+
+AppRegistry.registerComponent(appName, () => App);
 
 // AppRegistry.registerComponent('custom-component', () => CustomComponent);
